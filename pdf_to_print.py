c8a000cb2bdbd2830eca3d7e5d47cfa17860bff9
OgliariNatan <natanogliari@gmail.com>
2023-11-24 10:28:41 -0300

    add project

---
+++ pdf_to_print.py
@@ -0,0 +1,9 @@
+#####	NOME:				pdf_to_print.py
+#####	VERSÃO:				1.0
+#####	DESCRIÇÃO:			Coleta informações de um arquivo em pdf e imprime em uma declaração
+#####	DATA DA CRIAÇÃO:	24/11/2023
+#####	ESCRITO POR:		Natan Ogliari
+#####	E-MAIL:				natanogliari@gmail.com
+#####	DISTRO:				Ubuntu GNU/Linux 22.04
+#####	LICENÇA:			MIT license
+#####	PROJETO:			https://github.com/casa-inteligente/pdf_to_print
