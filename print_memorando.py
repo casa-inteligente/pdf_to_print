76e37b0306e2bf2255c5c7c819252e9537acb3f0
OgliariNatan <natanogliari@gmail.com>
2024-02-26 11:23:47 -0300

    add new varredura

--- print_memorando.py
+++ print_memorando.py
@@ -24,24 +24,24 @@ import sys
 class Template:
     def Imprimi_nova(self):
         lista_impressora = win32print.EnumPrinters(2)# Lista de impressoras no PC
-        for impressora in lista_impressora:
-            print(f'Lista a impressora {impressora}')#indice 0 da impressora
+        #for impressora in lista_impressora:
+            #print(f'Lista a impressora {impressora}')#indice 0 da impressora
         myImpressora = lista_impressora[0]
-        print(type(myImpressora))
-        print(myImpressora[2])
+       
         ### Adicionado para pagar a bandeja manual
         handle = win32print.OpenPrinter(myImpressora[2])
-        print(type(handle)
         
-        #properties = win32print.GetPrinter(handle, 2) #Usualmente '2' é a bandeja manual
-        #properties['pDevMode'].__dict__['BinSelection'] = 2
-       
-        #properties['pDevMode'].BinSelection = 2
+        
+        #property = win32print.GetPrinter(handle, 2) #Usualmente '2' é a bandeja manual
+        #property['pDevMode'].__dict__['BinSelection'] = 2
+        #print(property['pDevMode'])
+        #property['pDevMode'].BinSelection = 2
 
-        #win32print.SetPrinter(handle, 2, properties, 0)
-
-        win32print.ClosePrinter(handle) #Fecha a configuração
+        #win32print.SetPrinter(handle, 2, property, 0)
+        #win32print.ClosePrinter(handle) #Fecha a configuração
         ### Fim da bandeja manual
+
+
         win32print.SetDefaultPrinter(myImpressora[2])
 
         #seta a pasta e impressão
@@ -49,12 +49,49 @@ class Template:
         #print(caminho)
         lista_arq_print = os.listdir(caminho)
         for arquivo in lista_arq_print:
-            print("d")
+            print("Remover este a habilitar a linha abaixo para imprimir")
             #win32api.ShellExecute(0, "print", arquivo, None, caminho, 0)
     ### FIM ### def Imprimi_nova(self):
+            
+    def GeneratePDF(self, dados):
+        try:
+            pass
+        except:
+            print(f'Erro ao gerar o memorando {nome_interno}.pdf')
+class Le_pdf:
+
+    def abre_pdf(self, paginas='all'):
+        try:
+            self._diretorio_entrada = Path(r"\\10.40.22.35/Plantão/Para Impressão do termo de recebimento/")
+
+            for arquivo in self._diretorio_entrada.glob('*.pdf'):
+                print(arquivo)
+
+            self._lista_tabela = tabula.io.read_pdf(arquivo, pages=paginas)
+            self.numero_de_tabelas = len(self._lista_tabela)
+            print('Possui {} tabelas para uso e um cabechalho' .format(self.numero_de_tabelas-1))
+            return self._lista_tabela
+
+        except:
+            print('Erro ao abrir o arquivo {}'.format(arquivo))
+
+
+    def extrai_tabela(self, lista_tabela):
+        try:
+            pass
+        except:
+            print(f'Erro ao extrair daods da tabela {lista_tabela}')
 
 #print(sys.executable) #Imprimi o local do interpretador
 
 template = Template() #Instancia a classe Template
+le_pdf = Le_pdf()
+
+tabelas_lida = le_pdf.abre_pdf()
+var_vezes = len(tabelas_lida)
+for x in range(var_vezes):
+    print(tabelas_lida[x])
+    #dados_impri = le_pdf.extrai_tabela(tabelas_lida[x+1])
+
 
 template.Imprimi_nova()
