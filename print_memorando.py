294092b7d6f38b389338d327888313e48cd42cb0
OgliariNatan <natanogliari@gmail.com>
2024-02-26 09:19:39 -0300

    adicionado scripty para impressão de momorandos

---
+++ print_memorando.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+#####	NOME:				print_memorando.py
+#####	VERSÃO:				1.0.1
+#####	DESCRIÇÃO:			Coleta informações de um arquivo em pdf e imprime memorando com nome e identificadores.
+#####	DATA DA CRIAÇÃO:	26/02/2024
+#####	ESCRITO POR:		Natan Ogliari
+#####	E-MAIL:				natanogliari@gmail.com
+#####	DISTRO:				Ubuntu GNU/Linux 22.04
+#####	LICENÇA:			MIT license
+#####	PROJETO:			https://github.com/casa-inteligente/pdf_to_print
+
