4683f5db0a55e3ea554e84882513c631b5e6003d
OgliariNatan <natanogliari@gmail.com>
2024-03-22 10:41:13 -0300

    alterado para a biblioteca pdfplumber

--- junta-tabelas-sequencia.py
+++ junta-tabelas-sequencia.py
@@ -22,6 +22,8 @@ from reportlab.lib.units import cm
 import sys
 import pandas as pd
 import pdfplumber
+import tkinter as tk
+from tkinter import filedialog
 
 class Template:
     def Imprimi_nova(self):
@@ -58,111 +60,7 @@ class Template:
             
     def GeneratePDF(self):
         try:
-            self._page_size = A4 # Define o tamenho da folha
-            self.pdf_filename = le_pdf.get_nome_interno() + ".pdf"
-            self.nome_arq_out = f'{le_pdf.get_dir_saida()}\{self.pdf_filename}'
-            #print(self.nome_arq_out)
-            c = canvas.Canvas(self.nome_arq_out, pagesize=self._page_size)
-            c.setTitle("Termo higiene")
-            c.setAuthor("Natan Ogliari")
-            #################################################################################################
-            ### Cabeçalho
-            c.setFillColor(aColor='black', alpha=.8)  # Cor Cinza
-            c.setFont("Helvetica-Oblique", 10)
-            c.drawImage('figure/logo.png', 2 * cm, 760, width=60, height=60) #Logo do estado
-            c.drawString(4.5 * cm, 800, "ESTADO DE SANTA CATARINA")
-            c.drawString(4.5 * cm, 785, "SECRETARIA DE ESTADO DA ADMINISTRAÇÃO PRISIONAL E SOCIOEDUCATIVA")
-            c.drawString(4.5 * cm, 770, 'PRESÍDIO MARAVILHA')
-            ################
-            #### Rodapé
-            # c.setFillColor(aColor='gray')  # Cor Cinza
-            c.setFillColor(aColor='black', alpha=.8)
-            # c.setFillColorRGB(.662 ,.662, .662, 1)
-            c.setFont("Helvetica-Oblique", 8)
-            c.drawCentredString(300, 70, 'POLÍCIA PENAL DE SANTA CATARINA', mode=None, charSpace=0)
-            c.drawCentredString(300, 60, 'Presídio de Maravilha', mode=None, charSpace=0)
-            c.drawCentredString(300, 50, 'Av. Sul Brasil, n. 1607, centro - CEP 89874-000 - Maravilha/SC', mode=None, charSpace=0)
-            c.drawCentredString(300, 40, 'Fone: (49) 3664 - 6672 / e-mail: pr29@pp.sc.gov.br', mode=None, charSpace=0)
-
-            ### Titulo do documento
-            c.setFillColor(aColor='black')  # Cor preto
-            c.setFont("Helvetica-Bold", 12)  # Negrito
-            c.drawCentredString(10.5 * cm, 730, 'TERMO DE RECEBIMENTO')
-            c.drawString(2 * cm, 680, 'Instruções:')
-            c.setFont("Helvetica-Oblique", 12, leading=1)  # Fonte normal
-            c.drawString(2 * cm, 650, '01) Os internos devem assinalar a opção com um "X";')
-            c.drawString(2 * cm, 630, '02) Na opção "Observação" descrever o que está sendo entregue (tamanho e quantidade')
-            c.drawString(2 * cm, 610, 'de uniforme);')
-            c.drawString(2 * cm, 590, '03) O termo de recebimento, devidamente assinado, deverá ser digitalizado e arquivado')
-            c.drawString(2 * cm, 570, 'no IPEN do interno;')
-            c.rect(2 * cm, 10 * cm, 17 * cm, 9 * cm, fill=0)  # para criar retangulo
-
-            ## data do sistema no formato BR
-            hoje = dd.datetime.now()
-            hoje_br = hoje.strftime("%d/%m/%Y")
-            # print(hoje_br)
-            # Componente no nome
-            c.rect(2 * cm, 19 * cm, 17 * cm, .5 * cm, fill=0)  # para criar retangulo
-            c.drawString(2 * cm, 19.1 * cm, f'  INTERNO: {le_pdf.get_nome_interno()}')
-            # Componente Ipem
-            c.rect(2 * cm, 18.5 * cm, 8.5 * cm, .5 * cm, fill=0)  # para criar retangulo
-            c.drawString(2 * cm, 18.6 * cm, f'  IPEN:      {le_pdf.get_numero_ipen()}')
-            # Componente data
-            c.rect(10.5 * cm, 18.5 * cm, 8.5 * cm, .5 * cm, fill=0)  # para criar retangulo
-            c.drawString(10.5 * cm, 18.6 * cm, f'    DATA:  {hoje_br}')
-            ## Componentes das informaçõe das coisas entregues
-            ## Componente do kit higiene
-            c.rect(2 * cm, 18 * cm, 4 * cm, .5 * cm, fill=0)  # para criar retangulo
-            c.rect(6 * cm, 18 * cm, 13 * cm, .5 * cm, fill=0)  # para criar retangulo
-            c.drawString(6 * cm, 18.1 * cm, ' Kit higiene')
-            ## Componente Uniforme
-            c.rect(2 * cm, 17.5 * cm, 4 * cm, .5 * cm, fill=0)  # para criar retangulo
-            c.rect(6 * cm, 17.5 * cm, 13 * cm, .5 * cm, fill=0)  # para criar retangulo
-            c.drawString(6 * cm, 17.6 * cm, ' Uniforme')
-            # Componente cobertor
-            c.rect(2 * cm, 17 * cm, 4 * cm, .5 * cm, fill=0)  # para criar retangulo
-            c.rect(6 * cm, 17 * cm, 13 * cm, .5 * cm, fill=0)  # para criar retangulo
-            c.drawString(6 * cm, 17.1 * cm, ' Cobertor')
-
-            # Componente Observação
-            c.setFillColor(aColor='black')  # Cor preto
-            c.setFont("Helvetica-Bold", 12)  # Negrito
-            c.rect(2 * cm, 16.5 * cm, 4 * cm, .5 * cm, fill=0)  # para criar retangulo
-            c.drawString(2 * cm, 16.6 * cm, '    Observação:')
-            c.rect(6 * cm, 16.5 * cm, 13 * cm, .5 * cm, fill=0)  # para criar retangulo
-
-            c.setFillColor(aColor='black')  # Cor preto
-            c.setFont("Helvetica-Oblique", 12)
-            c.rect(2 * cm, 16 * cm, 17 * cm, .5 * cm, fill=0)  # para criar retangulo
-            c.drawString(2 * cm, 16.1 * cm, '  Se uniforme, quantidade e tamanho.')
-            c.rect(2 * cm, 15.5 * cm, 17 * cm, .5 * cm, fill=0)  # para criar retangulo
-            c.drawAlignedString(15 * cm, 15.6 * cm, "KIT DE HIGIENE: 01 ESCOVA DE DENTE, 01 CREME DENTAL, 03 ROLOS DE PAPEL")
-            c.drawAlignedString(18.5 * cm, 15.1 * cm, "HIGIÊNICO, 04 BARBEADORES, 01 DESODORANTE LIQUIDO E 01 SABONETE.    ")
-            c.rect(2 * cm, 15 * cm, 17 * cm, .5 * cm, fill=0)  # para criar retangulo
-            c.rect(2 * cm, 14.5 * cm, 17 * cm, .5 * cm, fill=0)  # para criar retangulo
-            c.rect(2 * cm, 14 * cm, 17 * cm, .5 * cm, fill=0)  # para criar retangulo
-
-            c.setFont("Helvetica-Oblique", 12)
-            ## Componente recebido em
-            c.setFillColor(aColor='black')  # Cor preto
-            c.setFont("Helvetica-Bold", 12)  # Negrito
-            c.drawString(2 * cm, 220, 'Recebido em ______/______/__________.')
-
-            ## Assinaura do interno
-            c.setFillColor(aColor='black')  # Cor preto
-            c.setFont("Helvetica-Bold", 12)  # Negrito
-            c.drawString(2 * cm, 170, '___________________________________')
-            c.drawString(2 * cm, 155, le_pdf.get_nome_interno())
-
-            ## Assinatura do servidor
-            c.setFillColor(aColor='black')  # Cor preto
-            c.setFont("Helvetica-Bold", 12)  # Negrito
-            c.drawString(2 * cm, 110, '___________________________________')
-            c.drawString(2 * cm, 95, 'Assinatura do servidor (nome completo).')
-
-            c.showPage()
-            c.save()  # Salva o documento e fecha
-            ##################################################################################################
+            pass
                     
 
         except:
@@ -177,76 +75,47 @@ class Le_pdf:
         return self._numero_ipen
     def get_dir_saida(self):
         return self._diretorio_saida
+    def get_dir_arq(self):
+        return self._arquivo
 
     def abre_pdf(self, paginas='all'):
         try:
-            self._diretorio_entrada = Path(r"\\10.40.22.35/Plantão/Para Impressão do termo de recebimento/")
+            #self._diretorio_entrada = Path(r"\\10.40.22.35/Plantão/Para Impressão do termo de recebimento/")
 
-            for arquivo in self._diretorio_entrada.glob('*.pdf'):#Arquivo .pdf a ser analisado
-                print(arquivo)
+            #for arquivo in self._diretorio_entrada.glob('*.pdf'):#Arquivo .pdf a ser analisado
+                #print(arquivo)
 
-            with pdfplumber.open(arquivo) as pdf:
-                 primeira_pagina = pdf.pages[0]
+            root.withdraw()
+            self._arquivo = filedialog.askopenfilename() # escolhe o arquivo
+            print('=================')
+            print(self._arquivo)
+            print('==================')
 
-                 tabelas = primeira_pagina.extract_tables()
+            with pdfplumber.open(self._arquivo) as pdf:
+                 
+                 self._all_tables = []
 
-                 for tabela in tabelas:
-                    df = pd.DataFrame(tabela)
-                    print(df)
+                 for pagina in pdf.pages:
+                    self._tables = pagina.extract_tables()
+
+                    for table in self._tables:
+                        self._all_tables.append(table)
             
-            #self._lista_tabela = tabula.io.read_pdf(arquivo, pages=paginas)
-            #self.numero_de_tabelas = len(self._lista_tabela)
-            #print('Possui {} tabelas para uso e um cabechalho' .format(self.numero_de_tabelas-1))
-            return self._lista_tabela
+                 #print (self._all_tables)
+            return self._all_tables
 
         except:
-            print('Erro ao abrir o arquivo {}'.format(arquivo))
+            print('Erro ao abrir o arquivo {}'.format(self._arquivo))
 
 
     def extrai_tabela(self, tabela):
         
         try:
-            
-            print('--esta na função extrai_tabelas----')
-            
-            if tabela.empty:
-                                print('Erro: A tabela está vazia.')
-                #return None
-            if 'PRONTUÁRIO | NOME' not in tabela.columns:
-                # Adiciona uma nova linha com valores padrão
-                print('=====================================')
-                print('++++Esta no adiciona linha nova++++++')
-                print('===================================')
-                print(tabela)
-                
-                #ADICIONAR CODIFICAÇÃO AQUI
-                
-                
-                new_row = {'PRONTUÁRIO | NOME': '123456 - Não Usar'}
-                print(new_row)
-                tabela = tabela.append(new_row, ignore_index=True)
-                print(tabela)
-                tabela = tabela['PRONTUÁRIO | NOME'].str.split(' - ', expand=True) #Cria duas colunas 
-                tabela = tabela.rename(columns={0: 'IPEN'})#Altera o nome da coluna
-                tabela = tabela.rename(columns={1: 'Nomes'})#Altera o nome da coluna
-                tabela['Nomes'] = tabela['Nomes'].replace(to_replace=r'\r', value=' ', regex=True)#remove o \r
-            else:
-                print("Entro na tabela que contem o cabechalho")
-                tabela = tabela['PRONTUÁRIO | NOME'].str.split(' - ', expand=True) #Cria duas colunas 
-                tabela = tabela.rename(columns={0: 'IPEN'})#Altera o nome da coluna
-                tabela = tabela.rename(columns={1: 'Nomes'})#Altera o nome da coluna
-                tabela['Nomes'] = tabela['Nomes'].replace(to_replace=r'\r', value=' ', regex=True)#remove o \r
+            df  = pd.concat([pd.DataFrame(tabela) for tabela in self._all_tables])
+            self._frist_colu = df.iloc[:,0]
+            print(self._frist_colu)
 
-            self._crit_stop = tabela.index.stop # Criterio de parada do for
-            self._diretorio_saida = Path(r'\\\\10.40.22.35/Plantão/Para Impressão do termo de recebimento/Imprimir/')# define o diretorio a ser gravado os arq pdf
-            self._diretorio_saida.mkdir(mode=777, parents=True, exist_ok=True) # Cria o diretorio caso não exista (Local inapropriado pois cria n vezes)
-        
-            for x in range(self._crit_stop): #Intera sobre todas as linha
-                self._nome_interno = tabela['Nomes'][x]
-                self._numero_ipen = tabela['IPEN'][x]
-                template.GeneratePDF()
-
-            return tabela
+          
 
         except :
             print('Erro ao extrair dados da tabela ', sys.exc_info()[0])
@@ -261,17 +130,18 @@ class Le_pdf:
 
 template = Template() #Instância a classe Template
 le_pdf = Le_pdf()
+root = tk.Tk()
 
 tabelas_lida = le_pdf.abre_pdf()
+le_pdf.extrai_tabela(tabelas_lida)
+
+#var_vezes = len(tabelas_lida) - 1 #Remove o cabechalho 
 
 
-var_vezes = len(tabelas_lida) - 1 #Remove o cabechalho 
-
-
-for x in range(var_vezes): #Intera sobre todas as tabelas
+#for x in range(var_vezes): #Intera sobre todas as tabelas
 #for x in range(1):
     #print(tabelas_lida[x+1])
-    dados_impri = le_pdf.extrai_tabela(tabelas_lida[x+1])
+    #dados_impri = le_pdf.extrai_tabela(tabelas_lida[x+1])
 
 #template.Imprimi_nova()
     
